8dbac18c29a7376d34b07aed8b1df845edb5b54f
jadenj3 <64673513+jadenj3@users.noreply.github.com>
2025-05-31 15:59:23 -0400

    fix import error

--- petl/vision_transformer_ssf.py
+++ petl/vision_transformer_ssf.py
@@ -33,7 +33,7 @@ import torch.utils.checkpoint
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
 from timm.models.helpers import build_model_with_cfg, named_apply, adapt_input_conv, resolve_pretrained_cfg, checkpoint_seq
 from timm.models.layers import DropPath, trunc_normal_, lecun_normal_, _assert
-from timm.models.layers.helpers import to_2tuple
+from timm.layers.helpers import to_2tuple
 from timm.models.registry import register_model
 
 
